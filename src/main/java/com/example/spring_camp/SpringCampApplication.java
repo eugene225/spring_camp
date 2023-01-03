7832c74e90b44efa5907a081ef3f41a571197cf8
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-01-03 16:34:50 +0900

    init spring

---
+++ src/main/java/com/example/spring_camp/SpringCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_camp/SpringCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
